90a0b22f9b53b777db353428f635307081aa1f1f
Lord Mike <lordmike@iki.fi>
2020-01-11 22:55:03 +0200

    Initial eclipse project code

---
+++ test_xml.py
@@ -0,0 +1,11 @@
+'''
+Created on 11 Jan 2020
+
+@author: lordmike
+'''
+
+import setup_apps
+
+if __name__ == '__main__':
+    print('Init messsage test_xml.py')
+    print('setup_apps.revision: ' + str(setup_apps.__revision__))
