39174ab8afe49b91e0fea61efec245ab86012d45
Lord Mike <lordmike@iki.fi>
2020-01-11 23:15:47 +0200

    Working XML read/write test

--- test_xml.py
+++ test_xml.py
@@ -9,3 +9,5 @@ import setup_apps
 if __name__ == '__main__':
     print('Init messsage test_xml.py')
     print('setup_apps.revision: ' + str(setup_apps.__revision__))
+    setup_apps.config.read_write()
+    print('END')
