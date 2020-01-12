7a3cdbfbc1e3c17543c2b113cb49ff481ebc9189
Lord Mike <lordmike@iki.fi>
2020-01-12 22:25:51 +0200

    Create sample xml with indent

--- test_xml.py
+++ test_xml.py
@@ -9,6 +9,6 @@ import setup_apps
 if __name__ == '__main__':
     print('Init messsage test_xml.py')
     print('setup_apps.revision: ' + str(setup_apps.__revision__))
-    setup_apps.config.read_write()
-    #setup_apps.config.create_test_xml()
+    #setup_apps.config.read_write()
+    setup_apps.config.create_test_xml()
     print('END')
