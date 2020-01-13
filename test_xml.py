f55694bef776ed7a6aa216bcaa0f4d7f29fa225e
Lord Mike <lordmike@iki.fi>
2020-01-13 21:14:51 +0200

    test the download function

--- test_xml.py
+++ test_xml.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     setup_apps.config.create_sample()
     setup_apps.config.print_sample()
     setup_apps.config.parse()
+    setup_apps.config.download()
     print('END')
