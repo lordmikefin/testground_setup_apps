44f95f385aab3d1b15b6ace4ed4c200db09e81da
Lord Mike <lordmike@iki.fi>
2020-01-13 18:11:33 +0200

    create the config xml file under user's home.

--- test_xml.py
+++ test_xml.py
@@ -10,5 +10,6 @@ if __name__ == '__main__':
     print('Init messsage test_xml.py')
     print('setup_apps.revision: ' + str(setup_apps.__revision__))
     #setup_apps.config.read_write()
-    setup_apps.config.create_test_xml()
+    #setup_apps.config.create_test_xml()
+    setup_apps.config.create_sample()
     print('END')
