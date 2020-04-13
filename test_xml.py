5aa89f9de18d4a87e077bc9be1946e31a782efa4
Lord Mike <lordmike@iki.fi>
2020-04-14 00:20:48 +0300

    Use logger instead of print

--- test_xml.py
+++ test_xml.py
@@ -9,13 +9,36 @@ import setup_apps
 from setup_apps import util
 #import json
 #import app_source_handler
+import logging
+import sys
+
+# Default log level.
+logging.basicConfig(level=logging.DEBUG)
+
+#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+#formatter = logging.Formatter('%(name)-10s - %(levelname)-4s: %(message)s')
+formatter = logging.Formatter('[%(name)-10s] %(levelname)-4s: %(message)s')
+logger_conf = setup_apps.logger
+hand_stdout = logging.StreamHandler(stream=sys.stdout)
+hand_stdout.setLevel(logging.DEBUG)
+hand_stdout.setFormatter(formatter)
+hand_stderr = logging.StreamHandler(stream=sys.stderr)
+hand_stderr.setLevel(logging.ERROR)
+hand_stderr.setFormatter(formatter)
+logger_conf.addHandler(hand_stdout)
+logger_conf.addHandler(hand_stderr)
+
+logger = logging.getLogger('test_xml')
+logger.addHandler(hand_stdout)
+logger.addHandler(hand_stderr)
+logger.propagate = False
 
 SOURCE_PATH = util.fix_path(util.home_path() + '/LM_ToyBox/setup_apps')
 SOURCE_FILE = 'app_source.xml'
 
 if __name__ == '__main__':
-    print('Init messsage test_xml.py')
-    print('setup_apps.revision: ' + str(setup_apps.__revision__))
+    logger.info('Init messsage test_xml.py')
+    logger.info('setup_apps.revision: ' + str(setup_apps.__revision__))
 
     # NOTE: Just testing the 'app_source_handler'
     #update_app_source.source.parse(source_file)
@@ -33,4 +56,4 @@ if __name__ == '__main__':
     setup_apps.config.download()
     setup_apps.config.install()
     setup_apps.config.configure()
-    print('END')
+    logger.info('END')
