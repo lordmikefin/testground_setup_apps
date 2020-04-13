fd9d460b52604ddd85bc6d55c40344b6e7fc39eb
Lord Mike <lordmike@iki.fi>
2020-04-13 19:57:51 +0300

    All testings into separate fuctions.

--- test_util.py
+++ test_util.py
@@ -14,6 +14,58 @@ def hint_test(test: str) -> bool:
 '''
 
 
+def init_testing():
+    print('')
+    print('Test function "python_version_str"')
+    test = util.python_version_str()
+    print('Python version: ' + str(test))
+
+    print('')
+    print('Test function "python_version"')
+    test = util.python_version()
+    print('Python version: ' + str(test))
+
+    # NOTE: This code is tested only with Python version 3.7
+    assert sys.version_info >= (3, 7)
+
+    print('')
+    print('Test function "hint_test"')
+    # TODO: why __annotations__ is marked as error in Eclipse?!
+    print('annotations: ' + str(util.hint_test.__annotations__))
+    #print('' + str(typing.get_type_hints(util.hint_test)))
+    #print('' + str(hint_test.__annotations__))
+    print('Call: ' + "hint_test('test')")
+    test = util.hint_test('test')
+    print('hint_test: ' + str(test))
+    print('Call: ' + "hint_test(1)")
+    test = util.hint_test(1)
+    print('hint_test: ' + str(test))
+
+    print('')
+    print('Test function "hint_test_complex"')
+    print('annotations: ' + str(util.hint_test_complex.__annotations__))
+    print('Call: ' + "hint_test_complex('test')")
+    test = util.hint_test_complex('test')
+    print('hint_test_complex: ' + str(test))
+    print('Call: ' + "hint_test_complex(1)")
+    test = util.hint_test_complex(1)
+    print('hint_test_complex: ' + str(test))
+
+    print('')
+    print('Test function "is_os_windows"')
+    test = util.is_os_windows()
+    print('This system is Windows: ' + str(test))
+
+    print('')
+    print('Test function "pause"')
+    util.pause()
+
+    print('')
+    print('Test function "home_path"')
+    home_dir = util.home_path()
+    print('The user home path: ' + str(home_dir))
+
+
 def run_command_testing():
     print('')
     print('Test function "run_os_command"')
@@ -91,56 +143,7 @@ if __name__ == '__main__':
     print('Init messsage test_util.py')
     print('setup_apps.revision: ' + str(setup_apps.__revision__))
 
-    print('')
-    print('Test function "python_version_str"')
-    test = util.python_version_str()
-    print('Python version: ' + str(test))
-
-    print('')
-    print('Test function "python_version"')
-    test = util.python_version()
-    print('Python version: ' + str(test))
-
-    # NOTE: This code is tested only with Python version 3.7
-    assert sys.version_info >= (3, 7)
-
-    print('')
-    print('Test function "hint_test"')
-    # TODO: why __annotations__ is marked as error in Eclipse?!
-    print('annotations: ' + str(util.hint_test.__annotations__))
-    #print('' + str(typing.get_type_hints(util.hint_test)))
-    #print('' + str(hint_test.__annotations__))
-    print('Call: ' + "hint_test('test')")
-    test = util.hint_test('test')
-    print('hint_test: ' + str(test))
-    print('Call: ' + "hint_test(1)")
-    test = util.hint_test(1)
-    print('hint_test: ' + str(test))
-
-    print('')
-    print('Test function "hint_test_complex"')
-    print('annotations: ' + str(util.hint_test_complex.__annotations__))
-    print('Call: ' + "hint_test_complex('test')")
-    test = util.hint_test_complex('test')
-    print('hint_test_complex: ' + str(test))
-    print('Call: ' + "hint_test_complex(1)")
-    test = util.hint_test_complex(1)
-    print('hint_test_complex: ' + str(test))
-
-    print('')
-    print('Test function "is_os_windows"')
-    test = util.is_os_windows()
-    print('This system is Windows: ' + str(test))
-
-    print('')
-    print('Test function "pause"')
-    util.pause()
-
-    print('')
-    print('Test function "home_path"')
-    home_dir = util.home_path()
-    print('The user home path: ' + str(home_dir))
-
+    init_testing()
     run_command_testing()
 
     print('')
