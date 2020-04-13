a8e9129326c30dedb91383534ace04d5bee3dcb8
Lord Mike <lordmike@iki.fi>
2020-04-13 18:58:38 +0300

    Moved run command testings into separate function.

--- test_util.py
+++ test_util.py
@@ -13,60 +13,8 @@ def hint_test(test: str) -> bool:
     return isinstance(test, str)
 '''
 
-if __name__ == '__main__':
-    print('Init messsage test_util.py')
-    print('setup_apps.revision: ' + str(setup_apps.__revision__))
 
-    print('')
-    print('Test function "python_version_str"')
-    test = util.python_version_str()
-    print('Python version: ' + str(test))
-
-    print('')
-    print('Test function "python_version"')
-    test = util.python_version()
-    print('Python version: ' + str(test))
-
-    # NOTE: This code is tested only with Python version 3.7
-    assert sys.version_info >= (3, 7)
-
-    print('')
-    print('Test function "hint_test"')
-    # TODO: why __annotations__ is marked as error in Eclipse?!
-    print('annotations: ' + str(util.hint_test.__annotations__))
-    #print('' + str(typing.get_type_hints(util.hint_test)))
-    #print('' + str(hint_test.__annotations__))
-    print('Call: ' + "hint_test('test')")
-    test = util.hint_test('test')
-    print('hint_test: ' + str(test))
-    print('Call: ' + "hint_test(1)")
-    test = util.hint_test(1)
-    print('hint_test: ' + str(test))
-
-    print('')
-    print('Test function "hint_test_complex"')
-    print('annotations: ' + str(util.hint_test_complex.__annotations__))
-    print('Call: ' + "hint_test_complex('test')")
-    test = util.hint_test_complex('test')
-    print('hint_test_complex: ' + str(test))
-    print('Call: ' + "hint_test_complex(1)")
-    test = util.hint_test_complex(1)
-    print('hint_test_complex: ' + str(test))
-
-    print('')
-    print('Test function "is_os_windows"')
-    test = util.is_os_windows()
-    print('This system is Windows: ' + str(test))
-
-    print('')
-    print('Test function "pause"')
-    util.pause()
-
-    print('')
-    print('Test function "home_path"')
-    home_dir = util.home_path()
-    print('The user home path: ' + str(home_dir))
-
+def run_command_testing():
     print('')
     print('Test function "run_os_command"')
     test = util.run_os_command('echo "TEST"')
@@ -132,6 +80,63 @@ if __name__ == '__main__':
     print('Test function "run_command_alt_1"')
     test = util.run_command_alt_1(['echo', 'TEST 1', '&&', 'echo', 'TEST 2'], shell=True)
     print('Command result: ' + str(test))
+
+
+if __name__ == '__main__':
+    print('Init messsage test_util.py')
+    print('setup_apps.revision: ' + str(setup_apps.__revision__))
+
+    print('')
+    print('Test function "python_version_str"')
+    test = util.python_version_str()
+    print('Python version: ' + str(test))
+
+    print('')
+    print('Test function "python_version"')
+    test = util.python_version()
+    print('Python version: ' + str(test))
+
+    # NOTE: This code is tested only with Python version 3.7
+    assert sys.version_info >= (3, 7)
+
+    print('')
+    print('Test function "hint_test"')
+    # TODO: why __annotations__ is marked as error in Eclipse?!
+    print('annotations: ' + str(util.hint_test.__annotations__))
+    #print('' + str(typing.get_type_hints(util.hint_test)))
+    #print('' + str(hint_test.__annotations__))
+    print('Call: ' + "hint_test('test')")
+    test = util.hint_test('test')
+    print('hint_test: ' + str(test))
+    print('Call: ' + "hint_test(1)")
+    test = util.hint_test(1)
+    print('hint_test: ' + str(test))
+
+    print('')
+    print('Test function "hint_test_complex"')
+    print('annotations: ' + str(util.hint_test_complex.__annotations__))
+    print('Call: ' + "hint_test_complex('test')")
+    test = util.hint_test_complex('test')
+    print('hint_test_complex: ' + str(test))
+    print('Call: ' + "hint_test_complex(1)")
+    test = util.hint_test_complex(1)
+    print('hint_test_complex: ' + str(test))
+
+    print('')
+    print('Test function "is_os_windows"')
+    test = util.is_os_windows()
+    print('This system is Windows: ' + str(test))
+
+    print('')
+    print('Test function "pause"')
+    util.pause()
+
+    print('')
+    print('Test function "home_path"')
+    home_dir = util.home_path()
+    print('The user home path: ' + str(home_dir))
+
+    run_command_testing()
 
     print('')
     print('')
