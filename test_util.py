21031e1e2b574ec803b7e0c880bf92cba4d87e89
Lord Mike <lordmike@iki.fi>
2020-04-13 14:51:13 +0300

    Test function "pause"

--- test_util.py
+++ test_util.py
@@ -10,10 +10,15 @@ from setup_apps import util
 if __name__ == '__main__':
     print('Init messsage test_util.py')
     print('setup_apps.revision: ' + str(setup_apps.__revision__))
+
     print('')
     print('Test function "is_os_windows"')
     test = util.is_os_windows()
     print('This system is Windows: ' + str(test))
+
+    print('')
+    print('Test function "pause"')
+    util.pause()
 
     print('')
     print('')
