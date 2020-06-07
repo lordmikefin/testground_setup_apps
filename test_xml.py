27d2af2d3305fcf674fa3643ddc9b60a2c061b66
Lord Mike <lordmike@iki.fi>
2020-06-07 15:33:19 +0300

    log init

--- test_xml.py
+++ test_xml.py
@@ -88,9 +88,12 @@ if __name__ == '__main__':
     #print('APPS: ' + json.dumps(app_source_handler.source.APPS, sort_keys=True, indent=2))
     if True:
         setup_apps.config.parse()
-    if False:
+    if True:
         setup_apps.config.init()
+    if False:
         setup_apps.config.download()
+    if False:
         setup_apps.config.install()
+    if False:
         setup_apps.config.configure()
     logger.info('END')
