87f467833576b3dbfcff4d75a440988a1f890d57
Lord Mike <lordmike@iki.fi>
2021-05-02 18:40:16 +0300

    ini config for testing

---
+++ config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2 May 2021
+
+@author: lordmike
+'''
+
+import configparser
+
+class Config(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
